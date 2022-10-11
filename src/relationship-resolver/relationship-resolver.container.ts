5ed016886c355f08164762c3b20939bf2cd256fb
Emir Alicic <emir@gradient.ba>
2022-10-11 11:29:10 +0200

    Formatting RelationshipResolverContainer

--- src/relationship-resolver/relationship-resolver.container.ts
+++ src/relationship-resolver/relationship-resolver.container.ts
@@ -1,23 +1,19 @@
-import { RelationshipResolver } from "../type/relationship-resolver";
+import { RelationshipResolver } from '../type/relationship-resolver';
 
 /**
  * Container that holds all registered relationship resolvers.
  */
 export class RelationshipResolverContainer {
-
   private relationshipResolvers: RelationshipResolver<unknown, unknown, unknown>[] = [];
 
-  async findRelationshipResolver<R>(requiredRelations: R[]):
-    Promise<RelationshipResolver<unknown, unknown, R> | null> {
-
+  async findRelationshipResolver<R>(requiredRelations: R[]): Promise<RelationshipResolver<unknown, unknown, R> | null> {
     let result = null;
 
     for (const relationshipResolver of this.relationshipResolvers as RelationshipResolver<unknown, unknown, R>[]) {
-
       const supportedRelations = await relationshipResolver.getSupportedRelations();
 
-      const matches = supportedRelations.find(sr => {
-        return !!requiredRelations.find(rr => rr === sr);
+      const matches = supportedRelations.find((sr) => {
+        return !!requiredRelations.find((rr) => rr === sr);
       });
 
       if (matches) {
