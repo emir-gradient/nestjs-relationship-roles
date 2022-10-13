5b35ad5224aa52c8953edc6adc3ccad0785ad0eb
Emir Alicic <emir@gradient.ba>
2022-10-13 10:19:41 +0200

    Renamed input paramters in mock relationship resolver

--- src/__tests__/mock/relationship-resolvers.ts
+++ src/__tests__/mock/relationship-resolvers.ts
@@ -3,18 +3,18 @@ import { BlogUser, Post } from './data-types';
 import { BlogRelationshipRole } from './roles';
 
 export class PostRelationshipResolver implements RelationshipResolver<BlogUser, Post, BlogRelationshipRole> {
-  getRelations(user: BlogUser, relatedObject: Post): Promise<BlogRelationshipRole[]> {
-    const supportedRoles: BlogRelationshipRole[] = [];
+  getRelations(user: BlogUser, post: Post): Promise<BlogRelationshipRole[]> {
+    const relationshipRoles: BlogRelationshipRole[] = [];
 
-    if (relatedObject.authorId === user.id) {
-      supportedRoles.push(BlogRelationshipRole.POST_AUTHOR);
+    if (post.authorId === user.id) {
+      relationshipRoles.push(BlogRelationshipRole.POST_AUTHOR);
     }
 
-    if (relatedObject.guestEditors.find((ge) => ge.id === user.id)) {
-      supportedRoles.push(BlogRelationshipRole.POST_GUEST_EDITOR);
+    if (post.guestEditors.find((ge) => ge.id === user.id)) {
+      relationshipRoles.push(BlogRelationshipRole.POST_GUEST_EDITOR);
     }
 
-    return Promise.resolve(supportedRoles);
+    return Promise.resolve(relationshipRoles);
   }
 
   getSupportedRelationships(): Promise<BlogRelationshipRole[]> {
