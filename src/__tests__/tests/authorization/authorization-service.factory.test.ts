dd4b19dfb6b9104e8f4364a2be9b474430c58bfa
Emir Alicic <emir@gradient.ba>
2022-10-13 13:49:44 +0200

    Authorization Service Factory included

--- src/__tests__/tests/authorization/authorization-service.factory.test.ts
+++ src/__tests__/tests/authorization/authorization-service.factory.test.ts
@@ -6,24 +6,21 @@ import {
   PostRelationshipResolver,
 } from '../../mock/relationship-resolvers';
 import { authorizationCases } from '../../test-cases/authorization-cases';
+import { getAuthorizationService } from '../../../authorization/authorization-service.factory';
 
 describe('RelationshipAuthorizationService', () => {
-  let relationshipAuthorizationService: RelationshipAuthorizationService;
-  let relationshipResolverContainer: RelationshipResolverContainer;
-
-  beforeEach(() => {
-    relationshipResolverContainer = new RelationshipResolverContainer();
-    relationshipAuthorizationService = new RelationshipAuthorizationService(relationshipResolverContainer);
-  });
-
   it('should properly allow access when there are no required roles', async () => {
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([]);
+
     await authorizationCases['should properly allow access when there are no required roles'](
       relationshipAuthorizationService,
     );
   });
 
   it('should allow access when user is in expected relation with post', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases['should allow access when user is in expected relation with post'](
       relationshipAuthorizationService,
@@ -31,7 +28,9 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should allow access when user is in at least one of the expected relations with post', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases['should allow access when user is in at least one of the expected relations with post'](
       relationshipAuthorizationService,
@@ -39,8 +38,10 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should allow access when user is in at least one relation with post - multiple resolvers', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostAuthorRelationshipResolver());
-    await relationshipResolverContainer.addRelationshipResolver(new PostGuestEditorRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostAuthorRelationshipResolver(),
+      new PostGuestEditorRelationshipResolver(),
+    ]);
 
     await authorizationCases[
       'should allow access when user is in at least one relation with post - multiple resolvers'
@@ -48,7 +49,9 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should deny access when user is not in any relation with post', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases['should deny access when user is not in any relation with post'](
       relationshipAuthorizationService,
@@ -56,7 +59,9 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should deny access when user is in wrong relation with post', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases['should deny access when user is in wrong relation with post'](
       relationshipAuthorizationService,
@@ -64,7 +69,9 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should allow access when user has more relations than requested - but including requested one', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases[
       'should allow access when user has more relations than requested - but including requested one'
@@ -72,7 +79,9 @@ describe('RelationshipAuthorizationService', () => {
   });
 
   it('should allow access when user has more relations and all are requested', async () => {
-    await relationshipResolverContainer.addRelationshipResolver(new PostRelationshipResolver());
+    const relationshipAuthorizationService: RelationshipAuthorizationService = await getAuthorizationService([
+      new PostRelationshipResolver(),
+    ]);
 
     await authorizationCases['should allow access when user has more relations and all are requested'](
       relationshipAuthorizationService,
