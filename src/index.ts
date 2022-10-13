ba7ae4de8c96ec6e4dc47bf4fe13d75e08b14159
Emir Alicic <emir@gradient.ba>
2022-10-13 13:52:16 +0200

    Ignore test coverage for index.ts - global exports file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// istanbul ignore file
 export * from './authorization/relationship-authorization.service';
 export * from './type/relationship-resolver';
 export * from './exception/supported-relationships-overlap.exception';
